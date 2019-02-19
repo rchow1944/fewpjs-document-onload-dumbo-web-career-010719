dfe4516194c80abc2a9be6331e605d1389fb81ae
rchow1944 <rchow2@buffalo.edu>
2019-02-19 17:02:29 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Your code goes here
+function updateDOM() {
+  document.getElementById('text').textContent = "This is really cool!";
+}
+
+function domContentLoaded() {
+  document.addEventListener("DOMContentLoaded", updateDOM, false);
+}
+
+domContentLoaded();
--- test/indexTest.js
+++ test/indexTest.js
@@ -10,6 +10,7 @@ describe("index.js", () => {
   it('should change the text on the page', function(){
     expect(document.querySelector('p').textContent).to.equal("JavaScript is so cool. It lets me add text to my page programmatically.");
     updateDOM()
+    // console.log(document.querySelector('p').textContent);
     expect(document.querySelector('p').textContent).to.equal("This is really cool!");
   });
 })
